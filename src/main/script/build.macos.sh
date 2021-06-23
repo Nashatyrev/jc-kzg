fcaac29deeb663146462995ea6c0da13b3f1d267
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2021-06-23 11:39:20 +0300

    Remove obsolete include dir for mac build

--- src/main/script/build.macos.sh
+++ src/main/script/build.macos.sh
@@ -33,6 +33,6 @@ swig -c++ -java -outdir src/generated/java/tech/pegasys/jckzg/swig -o src/genera
 
 # build JC-KZG shared library
 cd $REPO_DIR/build.native
-clang++ -O -fPIC -I../c-kzg/src -I../blst/bindings -I../src/main/cpp -I$JDK_DIR/include -I$JDK_DIR/include/linux -I$JDK_DIR/include/darwin -c ../src/generated/cpp/jc-kzg.cpp
+clang++ -O -fPIC -I../c-kzg/src -I../blst/bindings -I../src/main/cpp -I$JDK_DIR/include -I$JDK_DIR/include/darwin -c ../src/generated/cpp/jc-kzg.cpp
 # linking to libjc-kzg.so
 clang++ -shared -o ../src/generated/resources/x86_64/libjc-kzg.dylib -L. -lblst -lckzg jc-kzg.o
