eee23332813eff5d35761670a687199623b1d9af
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2021-07-05 16:36:41 +0300

    Update C-KZG submodule to head

--- src/main/cpp/poly.hpp
+++ src/main/cpp/poly.hpp
@@ -30,7 +30,7 @@ public:
 
 	Poly* long_div(Poly *divisor) throw(KZGException) {
 		Poly* ret = new Poly();
-		CKZG_TRY(new_poly_long_div(&ret->_poly, &_poly, &divisor->_poly));
+		CKZG_TRY(::poly_long_div(&ret->_poly, &_poly, &divisor->_poly));
 		return ret;
 	}
 
