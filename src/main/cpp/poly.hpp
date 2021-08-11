487467024ca6d334e9919f734ce212f6acf4ad61
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2021-08-11 13:16:49 +0300

    Fix Poly.long_div. Add Poly.mul() and coeff() methods

--- src/main/cpp/poly.hpp
+++ src/main/cpp/poly.hpp
@@ -24,21 +24,35 @@ public:
 	}
 
 	Poly(std::vector<Fr> coeff) throw(KZGException) {
-		CKZG_TRY(new_poly_with_coeffs(&_poly, (fr_t*)&coeff[0], coeff.size()));
+		fr_t* ptr = coeff.size() > 0 ? (fr_t*)&coeff[0] : NULL;
+		CKZG_TRY(new_poly_with_coeffs(&_poly, ptr, coeff.size()));
 	}
 	~Poly() {
 		free_poly(&_poly);
 	}
 
 	Poly* long_div(Poly *divisor) throw(KZGException) {
-		Poly* ret = new Poly();
+		Poly* ret = new Poly(_poly.length);
 		CKZG_TRY(::new_poly_div(&ret->_poly, &_poly, &divisor->_poly));
+		return ret;
+	}
+
+	Poly* mul(Poly* multiplicator) throw(KZGException) {
+		Poly* ret = new Poly(_poly.length + multiplicator->_poly.length);
+		CKZG_TRY(::poly_mul(&ret->_poly, &_poly, &multiplicator->_poly));
 		return ret;
 	}
 
 	Fr eval(const Fr x) {
 		Fr ret;
 		eval_poly(&ret.fr, &_poly, &x.fr);
+		return ret;
+	}
+
+	std::vector<Fr> coeff() {
+		Fr* start = (Fr*)_poly.coeffs;
+		Fr* end = (Fr*)(_poly.coeffs + _poly.length);
+		std::vector<Fr> ret(start, end);
 		return ret;
 	}
 
